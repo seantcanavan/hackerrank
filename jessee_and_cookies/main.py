31f956ddfa512957f8441e946012e39821b9fc94
seantcanavanpublic@gmail.com <seantcanavanpublic@gmail.com>
2022-03-21 08:56:19 -0500

    finish jessee and cookies

--- jessee_and_cookies/main.py
+++ jessee_and_cookies/main.py
@@ -1,36 +1,19 @@
-def find_index(cookie_sweetness_list, new_sweetness):
-    start = 0
-    end = len(cookie_sweetness_list) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if cookie_sweetness_list[mid] == new_sweetness:
-            return mid
-        elif cookie_sweetness_list[mid] > new_sweetness:
-            start = mid + 1
-        else:
-            end = mid - 1
-
-    # Return the index to insert at and add one to make it after the current 'end'
-    return end + 1
+from heapq import heapify, heappush, heappop
 
 
-def cookies(sweetness_threshold, cookie_sweetness_list):
-    # Start by sorting the cookies
-    cookie_sweetness_list.sort(reverse=True)
+def cookies(sweetness_threshold, cookie_sweetness_heap):
+    heapify(cookie_sweetness_heap)
     iterations = 0
 
-    # while the last value in the array is less than the minimum sweetness index
-    while cookie_sweetness_list[-1] < sweetness_threshold:
+    # peek the first value in the heap and check if it's lower than sweetness threshold
+    while cookie_sweetness_heap[0] < sweetness_threshold:
         # if we run out of cookies to mashup, we lose
-        if len(cookie_sweetness_list) < 2:
+        if len(cookie_sweetness_heap) < 2:
             return -1
-        first_least = cookie_sweetness_list.pop()
-        second_least = cookie_sweetness_list.pop()
+        first_least = heappop(cookie_sweetness_heap)
+        second_least = heappop(cookie_sweetness_heap)
         new_sweetness = combine_sweetness(first_least, second_least)
-        new_sweetness_index = find_index(cookie_sweetness_list, new_sweetness)
-        cookie_sweetness_list.insert(new_sweetness_index, new_sweetness)
+        heappush(cookie_sweetness_heap, new_sweetness)
         iterations += 1
     return iterations
 
